889fc75c096626233107eb635bd28d85d01e9f1b
Lucas <lucasmeurer96@gmail.com>
2024-09-16 09:21:32 +0000

    Initial commit

---
+++ src/app.rs
@@ -0,0 +1,26 @@
+#[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct App {}
+
+impl App {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::Window::new("Hello Egui").show(ctx, |ui| {
+            ui.label("Hello World!");
+        });
+    }
+}
